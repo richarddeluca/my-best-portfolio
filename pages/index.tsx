97c66ace85fbe966b99060af8c5a3a8997d16b6d
Richard Deluca <richard.deluca.dev@gmail.com>
2022-02-20 19:03:48 -0300

    Components folder location fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,10 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
-import { Footer } from './components/Footer'
-import { Header } from './components/Header'
-import { Headlines } from './components/Headlines'
-import { SignInButton } from './components/SignInButton'
-import { ThemeChanger } from './components/ThemeChanger'
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import { Headlines } from '../components/Headlines'
+import { ThemeChanger } from '../components/ThemeChanger'
 
 const Home: NextPage = () => {
   return (
